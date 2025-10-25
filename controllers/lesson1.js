53d527c16908eb0e5fe051a66c197643dd03527d
svpontes <svpontes2015@gmail.com>
2025-10-25 21:56:15 +0100

    add routes

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,32 @@ const sergioRoute = (req, res) => {
     res.send('Sergio Pontes');
 };
 
+const bernardoRoute = (req, res) => {
+    res.send('Sergio Pontes');
+};
+
+const estherRoute = (req, res) => {
+    res.send('Esther Pontes');
+};
+
+const muriloRoute = (req, res) => {
+    res.send('Murilo Pontes');
+};
+
+const arthurRoute = (req, res) => {
+    res.send('Arthur Pontes');
+};
+
+const barbaraRoute = (req, res) => {
+    res.send('Barbara Pontes');
+};
+
 module.exports = {
     tatyRoute,
     sergioRoute,
+    bernardoRoute,
+    muriloRoute,
+    estherRoute,
+    arthurRoute,
+    barbaraRoute,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,10 @@ const lesson1Controllers = require('../controllers/lesson1')
 
 routes.get('/', lesson1Controllers.tatyRoute);
 routes.get('/sergio', lesson1Controllers.sergioRoute);
+routes.get('/bernardo', lesson1Controllers.bernardoRoute);
+routes.get('/murilo', lesson1Controllers.muriloRoute);
+routes.get('/esther', lesson1Controllers.estherRoute);
+routes.get('/arthur', lesson1Controllers.arthurRoute);
+routes.get('/barbara', lesson1Controllers.barbaraRoute);
 
 module.exports = routes;
